3388f29dc04bb0c8c5961873319645212a0dbd8d
JoJosefe <147321018+Seifex01@users.noreply.github.com>
2024-01-17 22:54:30 +0100

    Modificacion de la actividad principal donde se ha corregido la funcion CuadriculaCard y se han terminado las funciones tanto CuadriculaApp como CuadriculaList. También se ha modificado la funcion CuadroCuadriculaPreview para obtener la vision de diseño de la funcion CuadriculaApp

--- app/src/main/java/com/example/cuadricula/MainActivity.kt
+++ app/src/main/java/com/example/cuadricula/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-
-
 import androidx.compose.foundation.layout.fillMaxSize
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
-
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
