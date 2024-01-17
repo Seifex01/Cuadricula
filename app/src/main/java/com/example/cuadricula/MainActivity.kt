efa40767f1b5e121689dfb5db85d36e852cd0831
JoJosefe <147321018+Seifex01@users.noreply.github.com>
2024-01-17 22:51:03 +0100

    Modificacion de la actividad principal donde se ha corregido la funcion CuadriculaCard y se han terminado las funciones tanto CuadriculaApp como CuadriculaList. También se ha modificado la funcion CuadroCuadriculaPreview para obtener la vision de diseño de la funcion CuadriculaApp

--- app/src/main/java/com/example/cuadricula/MainActivity.kt
+++ app/src/main/java/com/example/cuadricula/MainActivity.kt
@@ -4,20 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
+
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -32,8 +31,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cuadricula.data.DataSource
 import com.example.cuadricula.model.Topic
 import com.example.cuadricula.ui.theme.CuadriculaTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +46,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CuadriculaContent("Android")
+                    CuadriculaApp("Android")
                 }
             }
         }
@@ -53,11 +54,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CuadriculaContent(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CuadriculaApp(name: String, modifier: Modifier = Modifier) {
+    CuadriculaList(cuadriculaList = DataSource.topics)
 }
 
 @Composable
@@ -112,5 +110,22 @@ fun CuadriculaCard(topic: Topic, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun CuadroCuadriculaPreview() {
-    CuadriculaCard(Topic(R.string.photography, 321, R.drawable.photography),)
+    //CuadriculaCard(Topic(R.string.photography, 321, R.drawable.photography),)
+    CuadriculaApp("Android")
+}
+
+@Composable
+fun CuadriculaList(cuadriculaList: List<Topic>, modifier: Modifier = Modifier){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier,
+        content = {
+            items(cuadriculaList) { topic ->
+                CuadriculaCard(
+                    topic = topic,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    )
 }
