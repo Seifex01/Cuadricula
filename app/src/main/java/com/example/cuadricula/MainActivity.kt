b98d733e2d42adcd25b4da03cc8d072dd3ef7821
JoJosefe <147321018+Seifex01@users.noreply.github.com>
2024-01-17 17:32:06 +0100

    Modificacion de la actividad principal en la que la "Card" se encuentra ya definida y con su diseño editado.

--- app/src/main/java/com/example/cuadricula/MainActivity.kt
+++ app/src/main/java/com/example/cuadricula/MainActivity.kt
@@ -3,13 +3,36 @@ package com.example.cuadricula
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cuadricula.model.Topic
 import com.example.cuadricula.ui.theme.CuadriculaTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,10 +60,57 @@ fun CuadriculaContent(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun CuadriculaCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier) {
+        Row (
+            modifier = Modifier
+        ){
+            Image(
+                painter = painterResource(topic.imageResourceId),
+                contentDescription = stringResource(topic.stringResourceId),
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .size(height = 68.dp, width = 68.dp),
+                contentScale = ContentScale.Crop,
+            )
+            Column(
+                modifier = Modifier
+                    .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                    .align(Alignment.CenterVertically),
+            ) {
+                Text(
+                    text = LocalContext.current.getString(topic.stringResourceId),
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.Start),
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically),
+                    )
+                    Text(
+                        text = topic.numeroTemas.toString(),
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                            .align(Alignment.CenterVertically),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CuadriculaTheme {
-        CuadriculaContent("Android")
-    }
+fun CuadroCuadriculaPreview() {
+    CuadriculaCard(Topic(R.string.photography, 321, R.drawable.photography),)
 }
